03c3560055aa5f89a0ed1fac0432283ee79865f2
Iain Liew <liewiain@gmail.com>
2024-07-09 11:13:48 +0800

    Update, delete, upload pic version

--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -1,12 +1,13 @@
 import { Button, Col, Image, Row } from "react-bootstrap";
 import { useContext, useState } from "react";
 import { useDispatch } from "react-redux";
-import { likePost, removeLikeFromPost } from "../features/posts/postsSlice";
+import { likePost, removeLikeFromPost, deletePost } from "../features/posts/postsSlice";
 import { AuthContext } from "./AuthProvider";
+import UpdatePostModal from "./UpdatePostModal";
 
 
 export default function ProfilePostCard({ post }) {
-    const { content, id: postId } = post;
+    const { content, id: postId, imageUrl } = post;
     const [likes, setLikes] = useState(post.likes || []);
     const dispatch = useDispatch();
     const { currentUser } = useContext(AuthContext);
@@ -14,6 +15,10 @@ export default function ProfilePostCard({ post }) {
     const isLiked = likes.includes(userId);
 
     const pic = "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
+
+    const [showUpdateModal, setShowUpdateModal] = useState(false);
+    const handleShowUpdateModal = () => setShowUpdateModal(true);
+    const handlseCloseUpdateModal = () => setShowUpdateModal(false);
 
     const handleLike = () => (isLiked ? removeFromLikes() : addToLikes());
 
@@ -27,6 +32,10 @@ export default function ProfilePostCard({ post }) {
         dispatch(removeLikeFromPost({ userId, postId }));
     };
 
+    const handleDelete = () => {
+        dispatch(deletePost({ userId, postId }));
+    };
+
     return (
         <Row className="p-3" style={{ borderTop: "1px solid #D3D3D3", borderBottom: "1px solid #D3D3D3" }}>
             <Col sm={1}>
@@ -37,6 +46,7 @@ export default function ProfilePostCard({ post }) {
                 <strong>Haris</strong>
                 <span> @haris.samingan | Apr 16</span>
                 <p>{content}</p>
+                <Image src={imageUrl} style={{ width: 150 }} />
                 <div className="d-flex justify-content-between">
                     <Button variant="light">
                         <i className="bi bi-chat"></i>
@@ -58,9 +68,17 @@ export default function ProfilePostCard({ post }) {
                     <Button variant="light">
                         <i className="bi bi-upload"></i>
                     </Button>
+                    <Button variant="light" >
+                        <i className="bi bi-pencil-square" onClick={handleShowUpdateModal}></i>
+                    </Button>
+                    <Button variant="light" >
+                        <i className="bi bi-trash" onClick={handleDelete}></i>
+                    </Button>
+                    <UpdatePostModal
+                        show={showUpdateModal} handleClose={handlseCloseUpdateModal} postId={postId} originalPostContent={content} />
                 </div>
             </Col>
 
         </Row>
-    )
+    );
 }
--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,6 +1,56 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { collection, doc, getDoc, getDocs, setDoc } from "firebase/firestore";
-import { db } from "../../firebase";
+import { collection, doc, getDoc, getDocs, setDoc, updateDoc, deleteDoc } from "firebase/firestore";
+import { db, storage } from "../../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+
+export const deletePost = createAsyncThunk(
+    "posts/deletePost",
+    async ({ userId, postId }) => {
+        try {
+            const postRef = doc(db, `users/${userId}/posts/${postId}`);
+            await deleteDoc(postRef);
+
+            return postId;
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+);
+
+export const updatePost = createAsyncThunk(
+    "posts/updatePosts",
+    async ({ userId, postId, newPostContent, newFile }) => {
+        try {
+            let newImageUrl;
+            if (newFile) {
+                const imageRef = ref(storage, `posts/${newFile.name}`);
+                const response = await uploadBytes(imageRef, newFile);
+                newImageUrl = await getDownloadURL(response.ref);
+            }
+
+            const postRef = doc(db, `users/${userId}/posts/${postId}`);
+            const postSnap = await getDoc(postRef);
+            if (postSnap.exists()) {
+                const postData = postSnap.data();
+                const updatedData = {
+                    ...postData,
+                    content: newPostContent || postData.content,
+                    imageUrl: newImageUrl || postData.imageUrl,
+                };
+
+                await updateDoc(postRef, updatedData);
+                const updatedPost = { id: postId, ...updatedData };
+                return updatedPost;
+            } else {
+                throw new Error("Post does not exist");
+            }
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+);
 
 export const fetchPostsByUser = createAsyncThunk(
     "posts/fetchByUser",
@@ -23,14 +73,23 @@ export const fetchPostsByUser = createAsyncThunk(
 
 export const savePost = createAsyncThunk(
     "posts/savePost",
-    async ({ userId, postContent }) => {
+    async ({ userId, postContent, file }) => {
 
         try {
+            let imageUrl = "";
+            console.log(file);
+            if (file !== null) {
+                const imageRef = ref(storage, `posts/${file.name}`);
+                const response = await uploadBytes(imageRef, file);
+                imageUrl = await getDownloadURL(response.ref);
+            }
+
+
             const postsRef = collection(db, `users/${userId}/posts`);
             console.log(`users/${userId}/posts`);
             const newPostRef = doc(postsRef);
             console.log(postContent);
-            await setDoc(newPostRef, { content: postContent, likes: [] });
+            await setDoc(newPostRef, { content: postContent, likes: [], imageUrl });
             const newPost = await getDoc(newPostRef);
 
             const post = {
@@ -119,6 +178,17 @@ const postsSlice = createSlice({
                 if (postIndex !== -1) {
                     state.posts[postIndex].likes = state.posts[postIndex].likes.filter((id) => id !== userId);
                 }
+            })
+            .addCase(updatePost.fulfilled, (state, action) => {
+                const updatedPost = action.payload;
+                const postIndex = state.posts.findIndex((post) => post.id === updatedPost.id);
+                if (postIndex !== -1) {
+                    state.posts[postIndex] = updatedPost;
+                }
+            })
+            .addCase(deletePost.fulfilled, (state, action) => {
+                const deletedPostId = action.payload;
+                state.posts = state.posts.filter((post) => post.id !== deletedPostId);
             });
     },
 });
